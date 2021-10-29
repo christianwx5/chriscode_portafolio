7e9eefcfadd2f0837337a271b551db34ad860e4d
Christian Di Santo <disantochistian@gmail.com>
2021-10-29 05:24:36 +0200

    portafalio restaurado

---
+++ portafolio.php
@@ -0,0 +1,80 @@
+<style>
+    @media (max-width:991px){
+      .AdaptMedia {
+      top: -15px;
+      }
+
+      .AdaptMedia2 {
+      top: 0px;
+      }
+      
+    }
+
+    @media (max-width:767px){
+      .AdaptMedia3 {
+      width: 100%;
+      }
+    }
+    
+</style>
+<section id="portfolio" class="gutter-medium " style="border-bottom: 1px solid #e5e5e5;">
+    <div class="container">
+        <div class="row">
+            <div class="wpb_column brando-column-container  vc_col-sm-12 vc_col-md-12 vc_col-xs-12 xs-text-center margin-six-bottom xs-margin-ten-bottom" data-front-class="brando-column-container  xs-text-center margin-six-bottom xs-margin-ten-bottom">
+                <div class="vc-column-innner-wrapper">
+                    <div class="wpd-innner-wrapper">
+                        <h4 class="brando-remove-frontend-editor-top alt-font  black-text heading-style11 ">Portafolio</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="wpb_column brando-column-container  vc_col-sm-12 vc_col-md-3 vc_col-xs-12 sm-text-center xs-margin-seven-bottom" data-front-class="brando-column-container  sm-text-center xs-margin-seven-bottom">
+                <div class="vc-column-innner-wrapper">
+                    <div class="wpd-innner-wrapper">
+                        <ul class="portfolio-filter sm-text-small yolo nav nav-tabs no-border portfolio-filter-tab-style-5 alt-font text-uppercase text-large brando-portfolio-1 AdaptMedia3">
+                            <li class="nav xs-display-inline xs-width-100 xs-text-center active">
+                                <a href="#" data-uniqueid="brando-portfolio-1" data-id="" data-filter="*" class="position-relative xs-display-inline AdaptMedia2">Todo</a>
+                            </li>
+                            <li class="nav xs-display-inline xs-width-100 xs-text-center">
+                                <a href="#" data-uniqueid="brando-portfolio-1" data-id="" data-filter=".portfolio-filter-50" class="position-relative xs-display-inline AdaptMedia">Web</a>
+                            </li>
+                            <li class="nav xs-display-inline xs-width-100 xs-text-center">
+                                <a href="#" data-uniqueid="brando-portfolio-1" data-id="" data-filter=".portfolio-filter-51" class="position-relative xs-display-inline AdaptMedia">Programas</a>
+                            </li>
+                            <li class="nav xs-display-inline xs-width-100 xs-text-center">
+                                <a href="#" data-uniqueid="brando-portfolio-1" data-id="" data-filter=".portfolio-filter-52" class="position-relative xs-display-inline AdaptMedia">Apps de Teléfono</a>
+                            </li>
+                            <li class="nav xs-display-inline xs-width-100 xs-text-center">
+                                <a href="#" data-uniqueid="brando-portfolio-1" data-id="" data-filter=".portfolio-filter-53" class="position-relative xs-display-inline AdaptMedia2 ">Juegos</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="wpb_column brando-column-container  vc_col-sm-12 vc_col-md-9 vc_col-xs-12" data-front-class="brando-column-container ">
+                <div class="vc-column-innner-wrapper">
+                    <div class="wpd-innner-wrapper">
+                        <div  class="work-2col">
+                            <div class=" gutter-medium portfolio-style-5">
+                                <div class="grid-gallery grid-style1 overflow-hidden">
+    
+
+                                    <div class="tab-content">
+                                        <ul class="masonry-items grid brando-portfolio-1" data-portfolio="" data-uniqueid="brando-portfolio-1">
+                                        <!--Todos los Items del portafolio-->
+                                            <?php include ("ItemsPort.html") ?>;
+
+                                            
+
+
+                                        <!--Todos los Items del portafolio fin-->
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
